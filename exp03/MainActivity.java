fdcc511809579e76cb902b1566440a1ccfaa23a9
陶应健 <34150956+T-times@users.noreply.github.com>
2019-04-04 12:12:59 +0800

    Add files via upload

---
+++ exp03/MainActivity.java
@@ -0,0 +1,19 @@
+package cn.itcast.myshoppingcart;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        Toast.makeText(this, "开始购物", Toast.LENGTH_SHORT).show();
+    }
+}
